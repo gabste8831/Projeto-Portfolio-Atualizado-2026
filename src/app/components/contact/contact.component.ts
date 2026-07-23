3bd7a49e01d35e1e1bc5669a012bb8a69384356b
Gabriel Steffens <gabrielsteffens2003@gmail.com>
2026-07-23 17:28:25 -0300

    reestruturação do email na seção de contato

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { PERFIL } from '../../data/portfolio-data';
 import { RevealDirective } from '../../shared/reveal.directive';
 
@@ -14,30 +14,18 @@ import { RevealDirective } from '../../shared/reveal.directive';
 export class ContactComponent {
   readonly perfil = PERFIL;
 
+  emailCopiado = signal(false);
+
   readonly redes = [
-    {
-      rotulo: 'LinkedIn',
-      href: this.perfil.linkedin,
-      iconeClass: 'fa-brands fa-linkedin-in',
-      externo: true
-    },
-    {
-      rotulo: 'GitHub',
-      href: this.perfil.github,
-      iconeClass: 'fa-brands fa-github',
-      externo: true
-    },
-    {
-      rotulo: 'Instagram',
-      href: this.perfil.instagram,
-      iconeClass: 'fa-brands fa-instagram',
-      externo: true
-    },
-    {
-      rotulo: 'E-mail',
-      href: `mailto:${this.perfil.email}`,
-      iconeClass: 'fa-solid fa-envelope',
-      externo: false
-    },
+    { rotulo: 'LinkedIn', href: this.perfil.linkedin, iconeClass: 'fa-brands fa-linkedin-in' },
+    { rotulo: 'GitHub', href: this.perfil.github, iconeClass: 'fa-brands fa-github' },
+    { rotulo: 'Instagram', href: this.perfil.instagram, iconeClass: 'fa-brands fa-instagram' },
   ];
+
+  copiarEmail(): void {
+    navigator.clipboard.writeText(this.perfil.email).then(() => {
+      this.emailCopiado.set(true);
+      setTimeout(() => this.emailCopiado.set(false), 1500);
+    });
+  }
 }
